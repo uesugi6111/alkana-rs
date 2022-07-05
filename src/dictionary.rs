69fee8c5714db803571768d3369455e8b6a1019e
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2022-07-05 21:39:45 +0900

    full dictionary

--- src/dictionary.rs
+++ src/dictionary.rs
@@ -1,12 +1,17 @@
 use std::collections::HashMap;
+use std::include_str;
 pub(crate) fn generate_dictionary() -> HashMap<String, String> {
+    let csv = include_str!("../dictionary.csv");
     let mut dictionary = HashMap::new();
 
-    dictionary.insert("zyuganov".to_string(), "ジュガノフ".to_string());
-    dictionary.insert("zygote".to_string(), "ザイゴウトゥ".to_string());
-    dictionary.insert("zygomatic".to_string(), "ザイゴウマティク".to_string());
-    dictionary.insert("zygoma".to_string(), "ザイゴウマ".to_string());
-    dictionary.insert("zvegintzov".to_string(), "ズベジンツフ".to_string());
+    for (al, kana) in csv.lines().map(|line| {
+        let mut parts = line.split(',');
+        let al = parts.next().unwrap();
+        let kana = parts.next().unwrap();
+        (al.to_owned(), kana.to_owned())
+    }) {
+        dictionary.insert(al, kana);
+    }
 
     dictionary
 }
